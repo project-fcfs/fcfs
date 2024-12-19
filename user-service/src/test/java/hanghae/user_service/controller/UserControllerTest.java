20be72a3fb11f29e5c54a4ff8133d2dc15e94d15
param <spsummerdoc@gmail.com>
2024-12-19 15:28:54 +0900

    test(userController): 유저 생성 및 인증코드 테스트 로직 추가

---
+++ user-service/src/test/java/hanghae/user_service/controller/UserControllerTest.java
@@ -0,0 +1,73 @@
+package hanghae.user_service.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import hanghae.user_service.controller.req.UserAuthCodeReqDto;
+import hanghae.user_service.controller.req.UserCreateReqDto;
+import hanghae.user_service.service.AuthenticationService;
+import hanghae.user_service.service.port.MailSenderHolder;
+import hanghae.user_service.testSupport.IntegrationInfraTestSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.ResultActions;
+
+class UserControllerTest extends IntegrationInfraTestSupport {
+
+    @MockitoBean
+    MailSenderHolder senderHolder;
+    @MockitoBean
+    AuthenticationService authenticationService;
+
+    @Test
+    @DisplayName("인증코드를 요청하면 인증번호를 받을 수 있다")
+    void requestCode() throws Exception {
+        // given
+        UserAuthCodeReqDto request = new UserAuthCodeReqDto("fatum4341@naver.com");
+
+        // when
+        ResultActions resultActions = mockMvc.perform(post("/authcode").content(om.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
+
+        // then
+        resultActions.andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("올바른 값을 입력하면 유저를 저장할 수 있다")
+    void createUser() throws Exception {
+        // given
+        UserCreateReqDto request = new UserCreateReqDto("123@naver.com", "내이름은",
+                "password", "address", "123456");
+
+        // when
+        ResultActions resultActions = mockMvc.perform(post("/signup").content(om.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print());
+
+        // then
+        resultActions.andExpect(status().isOk());
+    }
+
+    @Nested
+    @DisplayName("유효성 검사")
+    class InvalidBinding{
+
+        @Test
+        @DisplayName("")
+        void 이름_test() throws Exception {
+            // given
+
+            // when
+
+            // then
+        }
+    }
+
+}
---
+++ user-service/src/test/java/hanghae/user_service/infrastructure/common/SystemPersonalDataEncryptorTest.java
@@ -0,0 +1,31 @@
+package hanghae.user_service.infrastructure.common;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import hanghae.user_service.testSupport.IntegrationInfraTestSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class SystemPersonalDataEncryptorTest extends IntegrationInfraTestSupport {
+
+    @Autowired
+    private SystemPersonalDataEncryptor encryptor;
+
+    @Test
+    @DisplayName("같은 값을 입력하면 디코딩이 가능하다")
+    void decodeSameValue() throws Exception {
+        // given
+        String value = "dafasdfewifhewf";
+        String encodeData = encryptor.encodeData(value);
+        System.out.println("encodeData = " + encodeData);
+
+        // when
+        String result = encryptor.decodeData(encodeData);
+
+        // then
+        assertThat(result).isEqualTo(value);
+    }
+
+}
