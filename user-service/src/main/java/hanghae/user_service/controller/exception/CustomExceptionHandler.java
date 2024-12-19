1c3b8ae77ff5ce3011e65fc06a9fee9de762be3e
param <spsummerdoc@gmail.com>
2024-12-19 14:49:35 +0900

    fix(exceptionHandler): Advice를 쓰지 않던걸 수정

--- user-service/src/main/java/hanghae/user_service/controller/exception/CustomExceptionHandler.java
+++ user-service/src/main/java/hanghae/user_service/controller/exception/CustomExceptionHandler.java
@@ -11,9 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestController
+@RestControllerAdvice
 public class CustomExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(CustomExceptionHandler.class);
--- user-service/src/main/java/hanghae/user_service/controller/req/UserCreateReqDto.java
+++ user-service/src/main/java/hanghae/user_service/controller/req/UserCreateReqDto.java
@@ -9,13 +9,13 @@ public record UserCreateReqDto(
         @UserEmailUnique(message = "이메일은 공백이거나 중복일 수 없습니다")
         @Email(message = "올바른 이메일을 입력해주세요")
         String email,
-        @Pattern(regexp = "^[가-힣\\w]{2,10}",message = "이름은 영문, 한글, 숫자 2~10자만 입력해주세요")
+        @Pattern(regexp = "^[가-힣\\w]{2,10}", message = "이름은 영문, 한글, 숫자 2~10자만 입력해주세요")
         String name,
         @NotBlank
         String password,
         @NotBlank
         String address,
-        @Pattern(regexp = "^[\\d]{6,6}",message = "인증번호는 6자리입니다.")
+        @Pattern(regexp = "\\d{6}", message = "인증 코드는 6자리만 입력해주세요")
         String authCode
 ) {
 }
