41a67102cd0c424f734c1851ed9e268f059c674a
param <spsummerdoc@gmail.com>
2024-12-26 13:38:42 +0900

    refactor(gateway): 프로그래밍에서 환경설정 Route로 변경

--- gateway-service/src/main/java/hanghae/gateway_service/config/UserFilterConfig.java
+++ gateway-service/src/main/java/hanghae/gateway_service/config/UserFilterConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 
-@Configuration
+//@Configuration
 public class UserFilterConfig {
 
     @Bean
