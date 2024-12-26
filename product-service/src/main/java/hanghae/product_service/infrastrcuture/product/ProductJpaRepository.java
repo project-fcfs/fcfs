d179ec94b0f0913c8cafa6da675047c483db31d2
param <spsummerdoc@gmail.com>
2024-12-26 16:40:07 +0900

    fix(productRepository): 조건절에 id가 아닌 ProductId로 수정

--- product-service/src/main/java/hanghae/product_service/infrastrcuture/product/ProductJpaRepository.java
+++ product-service/src/main/java/hanghae/product_service/infrastrcuture/product/ProductJpaRepository.java
@@ -1,6 +1,5 @@
 package hanghae.product_service.infrastrcuture.product;
 
-import hanghae.product_service.domain.product.Product;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +9,6 @@ import org.springframework.data.repository.query.Param;
 public interface ProductJpaRepository extends JpaRepository<ProductEntity, Long> {
     Optional<ProductEntity> findByProductId(String productId);
 
-    @Query("select p from ProductEntity p where p.id in :ids")
-    List<ProductEntity> findAllByProductIds(@Param("ids") List<String> ids);
+    @Query("select p from ProductEntity p where p.productId in :ids")
+    List<ProductEntity> findAllByProductIds(@Param("productIds") List<String> productIds);
 }
