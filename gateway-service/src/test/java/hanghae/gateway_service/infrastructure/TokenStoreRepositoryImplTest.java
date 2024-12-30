c331d275784d755823ac04e861994df4d0398070
param <spsummerdoc@gmail.com>
2024-12-30 13:26:32 +0900

    test(token): 로그인 유저인지 확인하는 테스트 로직 추가

---
+++ gateway-service/src/test/java/hanghae/gateway_service/infrastructure/TokenStoreRepositoryImplTest.java
@@ -0,0 +1,38 @@
+package hanghae.gateway_service.infrastructure;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+class TokenStoreRepositoryImplTest {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    private TokenStoreRepositoryImpl tokenStoreRepository;
+
+    @Test
+    @DisplayName("만료기한이 지나면 자연스럽게 사라지고 없다고 판단한다")
+    void expirationTest() throws Exception {
+        // given
+        String key = "testKey";
+        String value = "testToken";
+        redisTemplate.opsForSet().add(key, value);
+        redisTemplate.expire(key, Duration.ofMillis(1L));
+
+        // when
+        boolean result = tokenStoreRepository.existLoginToken(key, value);
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+}
