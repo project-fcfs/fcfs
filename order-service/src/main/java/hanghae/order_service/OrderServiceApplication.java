c9a2c2c738880a1d82e6d7e87d8608f8bf47d803
param <spsummerdoc@gmail.com>
2025-01-02 11:37:33 +0900

    feat(log): zipkin 추적 빈 추가

--- order-service/src/main/java/hanghae/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/hanghae/order_service/OrderServiceApplication.java
@@ -1,6 +1,9 @@
 package hanghae.order_service;
 
+import feign.Capability;
 import feign.Logger;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -19,6 +22,11 @@ public class OrderServiceApplication {
     @Bean
     public Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public Capability capability(MeterRegistry registry) {
+        return new MicrometerCapability(registry);
     }
 
 }
