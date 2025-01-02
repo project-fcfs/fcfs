d06a310ac70b593977b66acff27a878974aba73e
param <spsummerdoc@gmail.com>
2025-01-02 12:02:14 +0900

    feat(log): zipkin 추적 빈 추가

--- product-service/src/main/java/hanghae/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/hanghae/product_service/ProductServiceApplication.java
@@ -1,17 +1,25 @@
 package hanghae.product_service;
 
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableKafka
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
+    @Bean
+    public Capability capability(MeterRegistry registry) {
+        return new MicrometerCapability(registry);
     }
 
 }
