28d607ff42284018e25b05c34e51bda583ddebdc
param <spsummerdoc@gmail.com>
2025-01-15 16:00:42 +0900

    test: 의존성 있는 테스트 제거

--- config-server/src/test/java/hanghae/config_server/ConfigServerApplicationTests.java
+++ config-server/src/test/java/hanghae/config_server/ConfigServerApplicationTests.java
@@ -3,11 +3,11 @@ package hanghae.config_server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/*@SpringBootTest
 class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
-}
+}*/
